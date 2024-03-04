aa1f59452fc6a000aad40501d74a25d36d624869
darshan <darsh2605@gmail.com>
2024-03-05 00:26:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/darshankomu/weather_flashcards_for_kids/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.darshankomu.weather_flashcards_for_kids
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+const kprimarycolor = Color(0xff1C3249);
+const ksecondcolor = Color(0xff3ba2ff);
+const kthirdcolor = Color(0xff006ac0);
+const ksplashback = Color(0xFFA48DC4);
+const kback = Color(0xFFA48DC4);
+const backcolor = Color(0xFFA48DC4);
+const backcolor_2 = Color(0xFFFFFFFF);
+
+
+
+var androidAppShareLink = "https://play.google.com/store/apps/details?id=com.darshankomu.weather_flashcards_for_kids";
+
+var androidShareText = "Download Weather for Kids app from below link";
+
+var androidAppIdValue = "com.darshankomu.weather_flashcards_for_kids";
+
+var iOSAppIdValue = "AddiOSAppId";
